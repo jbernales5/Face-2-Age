e4ce00462f2d1341cc0e1f3a46b0f4fd9c00357b
Jonathan B <me@jonathan-bernales.com>
2020-05-31 19:03:49 +0200

    Bugfix

--- dockerized/back/wsgi.py
+++ dockerized/back/wsgi.py
@@ -1,6 +1,6 @@
 from server import app, load_model_age_real, load_model_gender
 
 if __name__ == "__main__":
-    load_model_age_real()
+    load_model_age_apparent()
     load_model_gender()
     app.run()
