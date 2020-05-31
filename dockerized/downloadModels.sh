906c8315f7a405dce8e573500e505565ae9b5952
Jonathan B <me@jonathan-bernales.com>
2020-05-31 18:47:45 +0200

    BugFix missing download links on downloadModels.sh

--- dockerized/downloadModels.sh
+++ dockerized/downloadModels.sh
@@ -2,3 +2,5 @@
 wget -O back/age.prototxt https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/age.prototxt
 wget -O back/dex_chalearn_iccv2015.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/dex_chalearn_iccv2015.caffemodel
 wget -O back/ilsvrc_2012_mean.npy https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
+wget -O back/gender.prototxt https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/gender.prototxt
+wget -O back/gender.caffemodel https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/gender.caffemodel
